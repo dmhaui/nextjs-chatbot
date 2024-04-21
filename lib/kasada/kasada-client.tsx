18031cb74e2833ed4f3ef7835d5cbce480e372fa
Đinh Nhật Minh <147995815+dmhaui@users.noreply.github.com>
2024-04-21 22:56:23 +0700

    Update kasada-client.tsx

--- lib/kasada/kasada-client.tsx
+++ lib/kasada/kasada-client.tsx
@@ -21,10 +21,7 @@ export function KasadaClient() {
     });`.replace(/[\n\r\s]/g, '')
         }}
       ></script>
-      <Script
-        async={true}
-        src="/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3/p.js"
-      ></Script>
+
     </>
   )
 }
