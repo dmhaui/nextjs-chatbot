786dc4f61418a4e510a8debf10af6a5a12ba2e87
Đinh Nhật Minh <147995815+dmhaui@users.noreply.github.com>
2024-04-21 23:05:58 +0700

    Update kasada-client.tsx

--- lib/kasada/kasada-client.tsx
+++ lib/kasada/kasada-client.tsx
@@ -3,7 +3,7 @@ import Script from 'next/script'
 export function KasadaClient() {
   return (
     <>
-      {/* <script
+      <script
         dangerouslySetInnerHTML={{
           __html:
             `document.addEventListener('kpsdk-load', () => {window.KPSDK.configure([
@@ -24,7 +24,7 @@ export function KasadaClient() {
       <Script
         async={true}
         src="/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3/p.js"
-      ></Script> */}
+      ></Script>
     </>
   )
 }
